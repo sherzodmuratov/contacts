f44d4ed699ac41c20e93acab30b7728f82b27c83
Sherzod Muratov <sherzod.muratov@gmail.com>
2014-03-08 23:20:48 -0500

    Setup to load contacts into page.

---
+++ presenter.js
@@ -0,0 +1,12 @@
+/*
+loadContacts() should make AJAX call to load contacts.txt file into container div.
+No formatting needed.
+read http://stackoverflow.com/questions/14446447/javascript-read-local-text-file
+ */
+
+ function loadContacts() {
+ 	var data = // here data needs to be loaded
+ 	document.getElementById("container").innerHTML =  data;
+ }
+
+ loadContacts(); // executes function
